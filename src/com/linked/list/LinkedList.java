b4a9c13cdfbaceb849693ea19c58ce26662a0b9e
ashish27upadhyay94 <ashish27upadhyay94@gmail.com>
2019-10-06 22:04:01 +0530

    my initial commit

---
+++ src/com/linked/list/LinkedList.java
@@ -0,0 +1,43 @@
+package com.linked.list;
+
+public class LinkedList {
+
+	Node head;
+
+	public void createLinkedList() {
+
+		head = new Node(0);
+		Node one = new Node(1);
+		Node two = new Node(2);
+		Node three = new Node(3);
+		Node four = new Node(4);
+
+		head.next = one;
+		one.next = two;
+		two.next = three;
+		three.next = four;
+
+	}
+
+	public void interateLinkedList(Node head) {
+
+		while (head != null) {
+
+			System.out.println(head.data);
+			head = head.next;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LinkedList obj = new LinkedList();
+
+		obj.createLinkedList();
+
+		obj.interateLinkedList(obj.head);
+
+	}
+
+}
---
+++ src/com/linked/list/Node.java
@@ -0,0 +1,13 @@
+package com.linked.list;
+
+public class Node {
+	int data;
+	Node next;
+
+	public Node(int data) {
+		super();
+		this.data = data;
+		this.next = null;
+	}
+
+}
